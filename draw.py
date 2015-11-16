ca433f6e5d164dd09a2eaa2031c53a62e97ca329
kreolim <dslesnikov@gmail.com>
2015-11-17 00:03:54 +0500

    Implemented plotting density of points

---
+++ draw.py
@@ -0,0 +1,17 @@
+import numpy as np
+from matplotlib.pyplot import contourf, savefig, gcf
+
+M = np.loadtxt('points.dat', skiprows=1)
+
+bins_x = 500
+bins_y = 500
+H, xedges, yedges = np.histogram2d(M[:,0], M[:,1], [bins_x, bins_y])
+
+X, Y = np.meshgrid((xedges[1:] + xedges[:-1]) / 2,
+                   (yedges[1:] + yedges[:-1]) / 2)
+
+
+contourf(X, Y, H, cmap='bone')
+fig = gcf()
+fig.set_size_inches(16.2, 10.8)
+fig.savefig('density.png', dpi=100)
