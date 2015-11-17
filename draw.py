d4a5718ecd994c531df3302ab3694c4b96e86313
kreolim <dslesnikov@gmail.com>
2015-11-17 19:04:06 +0500

    Changed plotting parameters

--- draw.py
+++ draw.py
@@ -3,8 +3,8 @@ from matplotlib.pyplot import contourf, gcf
 
 M = np.loadtxt('points.dat', skiprows=1)
 
-bins_x = 500
-bins_y = 500
+bins_x = 600
+bins_y = 600
 H, xedges, yedges = np.histogram2d(M[:,0], M[:,1], [bins_x, bins_y])
 
 X, Y = np.meshgrid((xedges[1:] + xedges[:-1]) / 2,
@@ -13,5 +13,5 @@ X, Y = np.meshgrid((xedges[1:] + xedges[:-1]) / 2,
 
 contourf(X, Y, H, cmap='jet')
 fig = gcf()
-fig.set_size_inches(16.2, 10.8)
-fig.savefig('density.png', dpi=100)
+fig.set_size_inches(13.66, 7.68)
+fig.savefig('density.png', dpi=500)
